40943cfbac2973ef20b7da6ac55983f2cc732f65
Arexan-K <94745953+ArexanK@users.noreply.github.com>
2022-12-06 15:22:47 +0100

    add slider

--- new.js
+++ new.js
@@ -1,10 +1,65 @@
-// nav toggle - select button and links
+// nav toggle 
 let navToggle = document.querySelector("#navToggle")
 let nav = document.querySelector("#nav-links")
 
 
 
-//add event listener
+
 navToggle.addEventListener('click', () => {
     nav.classList.toggle('nav-open')
 })
+
+// slider container 
+
+jQuery(document).ready(function ($) {
+
+    $('#checkbox').change(function () {
+        setInterval(function () {
+            moveRight();
+        }, 2000);
+    });
+
+    var slideCount = $('#slider ul li').length;
+    var slideWidth = $('#slider ul li').width();
+    var slideHeight = $('#slider ul li').height();
+    var sliderUlWidth = slideCount * slideWidth;
+
+    $('#slider').css({
+        width: slideWidth,
+        height: slideHeight
+    });
+
+    $('#slider ul').css({
+        width: sliderUlWidth,
+        marginLeft: -slideWidth
+    });
+
+    $('#slider ul li:last-child').prependTo('#slider ul');
+
+    function moveLeft() {
+        $('#slider ul').animate({
+            left: +slideWidth
+        }, 200, function () {
+            $('#slider ul li:last-child').prependTo('#slider ul');
+            $('#slider ul').css('left', '');
+        });
+    };
+
+    function moveRight() {
+        $('#slider ul').animate({
+            left: -slideWidth
+        }, 200, function () {
+            $('#slider ul li:first-child').appendTo('#slider ul');
+            $('#slider ul').css('left', '');
+        });
+    };
+
+    $('a.control_prev').click(function () {
+        moveLeft();
+    });
+
+    $('a.control_next').click(function () {
+        moveRight();
+    });
+
+});
